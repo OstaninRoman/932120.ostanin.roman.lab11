d875009eb69b9303ddad4568c6fc6efe6a79a215
Roman <ostanin.roma15@gmail.com>
2024-12-24 15:52:57 +0700

    Добавил файлы task-11

---
+++ lab11/Models/CalcServiceModel.cs
@@ -0,0 +1,12 @@
+namespace Lab_11.Models
+{
+    public class CalcServiceModel
+    {
+        public int FirstValue { get; set; }
+        public int SecondValue { get; set; }
+        public int Add { get; set; }
+        public int Sub { get; set; }
+        public int Mult { get; set; }
+        public double? Div { get; set; }
+    }
+}
---
+++ lab11/Services/CalcService.cs
@@ -0,0 +1,17 @@
+﻿using Lab_11.Models;
+
+public class CalcService
+{
+    public CalcServiceModel Calculate(int firstValue, int secondValue)
+    {
+        return new CalcServiceModel
+        {
+            FirstValue = firstValue,
+            SecondValue = secondValue,
+            Add = firstValue + secondValue,
+            Sub = firstValue - secondValue,
+            Mult = firstValue * secondValue,
+            Div = secondValue != 0 ? (double?)(firstValue / secondValue) : null
+        };
+    }
+}
